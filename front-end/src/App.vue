843882adc3a152ecb97834216c4d2222cccb34de
Esteban <esteban93torres@gmail.com>
2018-08-25 11:12:18 -0500

    firstMap-sails-installation

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <app-map msg="Delktium"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import appMap from './components/app-map.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    appMap
   }
 }
 </script>
---
+++ front-end/src/components/app-map.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+   <GmapMap
+  :center="{lat:10, lng:10}"
+  :zoom="7"
+  map-type-id="terrain"
+  style="width: 500px; height: 300px"
+>
+  <GmapMarker
+    :key="index"
+    v-for="(m, index) in markers"
+    :position="m.position"
+    :clickable="true"
+    :draggable="true"
+    @click="center=m.position"
+  />
+</GmapMap>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+
+</style>
